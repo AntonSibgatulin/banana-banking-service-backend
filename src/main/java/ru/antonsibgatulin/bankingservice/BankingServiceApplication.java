53fa545b7aad648a5e70f8a41ab20546d55e2281
anton <89879639755a@gmail.com>
2024-03-05 21:49:38 +0300

    Включил таймер(Расписание) для увеличивания баланса

--- src/main/java/ru/antonsibgatulin/bankingservice/BankingServiceApplication.java
+++ src/main/java/ru/antonsibgatulin/bankingservice/BankingServiceApplication.java
@@ -2,7 +2,10 @@ package ru.antonsibgatulin.bankingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+@EnableScheduling
 @SpringBootApplication
 public class BankingServiceApplication {
 
