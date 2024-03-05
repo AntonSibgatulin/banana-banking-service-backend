6ed7f1b96a6eb83d2f04cd719d2cc1bc7ed975b1
anton <89879639755a@gmail.com>
2024-03-05 21:51:13 +0300

    Добавил конструкторы для TransactionService

--- src/main/java/ru/antonsibgatulin/bankingservice/dto/transaction/request/TransactionDto.java
+++ src/main/java/ru/antonsibgatulin/bankingservice/dto/transaction/request/TransactionDto.java
@@ -3,9 +3,11 @@ package ru.antonsibgatulin.bankingservice.dto.transaction.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@NoArgsConstructor
 @Data
 @Schema(description = "Transaction DTO")
 public class TransactionDto {
@@ -21,4 +23,13 @@ public class TransactionDto {
     @DecimalMin(value = "1.0", inclusive = false, message = "Amount must be greater than one")
     @Schema(description = "Amount to be transferred")
     private BigDecimal amount;
+
+
+
+    public TransactionDto(Long receivedWalletId, String phoneReceived, Long receivedUserId, BigDecimal amount) {
+        this.receivedWalletId = receivedWalletId;
+        this.phoneReceived = phoneReceived;
+        this.receivedUserId = receivedUserId;
+        this.amount = amount;
+    }
 }
